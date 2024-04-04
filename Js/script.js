262e4b255540250414d30529d70a290b74ac31b3
Dennis C. Mathews Jr. <dmathewsjr07106@gmail.com>
2024-04-04 16:30:03 -0400

    update files

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+// Declaring Variables
+const PI = 3.1415;
+const rad = 5;
+const space = 0.8;
+const area = PI * (rad * rad);
+const totalPlant = Math.floor(area * space)
+let plant = 20;
---
+++ Js/scripts.js
@@ -0,0 +1,40 @@
+// Declaring my variables for Section 1 1A
+const myName = 'Dennis Mathews Jr.';
+let favoriteLang = 'Python';
+let newfavoriteLang = 'Javascript';
+favoriteLang = newfavoriteLang;
+// Printing the message after manupalation of data
+console.log('Hello my name is ' + myName + ' and my favorite language is ' + favoriteLang);
+
+// Declaring Variables for 1B
+let firstVar = 'Hello World';
+firstVar = 5;
+let secondVar = firstVar;
+secondVar = 'Bye-Bye'
+//  Printing the message after manupalation of data 
+console.log(firstVar);
+// Printing out a Greeting
+const yourName = 'Dennis Mathews Jr.';
+console.log('Hello, my name is ' + myName);
+
+// Declaring variables for 1C
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+// Make Booleans print true
+
+  console.log(a < b);
+  console.log(c > d);
+  console.log('Name' == 'Name'); 
+  // FOR THE NEXT TWO, USE ONLY && OR ||
+  console.log(true || false);
+  console.log(false && false && false && false && false || true);
+  console.log(false == false)
+  console.log(e === 'Kevin');
+  console.log(a && b !== c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+  console.log(a + a !== d); // note: the answer is a simple arithmetic equation, not something "weird"
+  console.log(48 == '48');
+
+

91912880e3e5869c0b35ff561d0239cd528e0e0c
Dennis C. Mathews Jr. <dmathewsjr07106@gmail.com>
2024-04-11 22:43:13 -0400

    Update Files

--- Js/scripts.js
+++ Js/scripts.js
@@ -299,3 +299,128 @@ console.log(myArray);
 // Yes a reference to same array 
 
 // Section 3H
+// Declaring Variables
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "Per Scholas hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+
+
+// What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+// let warobe = kristynsCloset.join('');
+console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!");
+// Kristyn just bought some sweet shades! Add "raybans"to her closet after "yellow knit hat".
+let addThings = kristynsCloset.splice(6, 0, 'raybans');
+console.log(kristynsCloset);
+// Kristyn spilled coffee on her hat... modify this item to read "stained knit hat"instead of yellow.
+kristynsCloset[5] = 'stained knit hat';
+console.log(5);
+// Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirtsarray.
+
+console.log(thomsCloset[0][0]);
+// In the same way, access one item from Thom's pants array.
+console.log(thomsCloset[1][0]);
+// Access one item from Thom's accessories array.
+console.log(thomsCloset[2][0]);
+// Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+ console.log("Thom is looking fierce in a " + thomsCloset[0][0] + ", " + thomsCloset[1][1] + " and " + thomsCloset[2][1]  + ".");
+// Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+thomsCloset[1][2] = 'Footie Pajamas';
+console.log(thomsCloset[1][2]);
+
+// Section 5 Objects
+// Let's set up an object data structure. Let's say we have a website that sells products, and we have a user of our website, and we want to store that user's data. The object data structure is a good way to organize the data from our user.
+// Declaring Object 5A
+// A. Make a user object Create an object called user. Write in to the object the key-value pairs for name, email, age, and purchased. Set the value of purchased to an empty array []. Set the other values to whatever you would like.
+const user = {
+    name: 'Junior',
+    email: 'JMccarter@gmail.com',
+    age: 25,
+    purchased: [],
+};
+// B. Update the user
+// Our user has changed his or her email address. Without changing the original user object, update the email value to a new email address.
+user.email = 'JMccarter07108@gmail.com';
+// Our user has had a birthday! Without changing the original user object, increment the age value using the postfix operator. Hint: age++
+let nAge = 0;
+nAge++;
+user.age = user.age + nAge;
+console.log(user.age);
+// C. Adding keys and values
+// You have decided to add your user's location to the data that you want to collect.
+user.location = '';
+console.log(user);
+
+// Without changing the original user object, add a new key location to the object, and give it a value or some-or-other location (a string).
+user.location = 'NJ';
+// D. Shopaholic!
+// Our user has purchased an item! They have purchased some "carbohydrates". Using .push(), add the string "carbohydrates" to the purchased array.
+user.purchased.push('carbohydrates');
+// Our user has purchased an item! They have purchased some "peace of mind". Using .push(), add the string "" to the purchased array.
+user.purchased.push('peace of mind');
+// Our user has purchased an item! They have purchased some "Merino jodhpurs". Using .push(), add the string "Merino jodhpurs" to the purchased array.
+user.purchased.push('Merino jodhpurs');
+console.log(user.purchased);
+// Console.log just the "Merino jodhpurs" from the purchased array.
+console.log(user.purchased[2]);
+// E. Object-within-object
+// Remember that you can add an object to an existing object in the same way that you can add any new property/value pair.
+
+// If we want to give our user a friend with a name and age, we could write:
+
+ user.friend = {
+     name: "Grace Hopper",
+     age: 85,
+     location: 'MD',
+     purchased: []
+};
+// When we console.log user, we would see the friend object added to our user object.
+
+// Write a friend object into your user object and give the friend a name, age, location, and purchased array (empty for now)
+// Console.log just the friend's name
+console.log(user.friend.name);
+
+// Console.log just the friend's location
+console.log(user.friend.location);
+// CHANGE the friend's age to 55
+user.friend.age = 55;
+// The friend has purchased "The One Ring". Use ``.push()to add "The One Ring" to the friend'spurchased` array.
+user.friend.purchased.push('The One Ring');
+// The friend has purchased "A latte". Use .push() to add "A latte" to the friend's purchased array.
+user.friend.purchased.push('A latte');
+// Console.log just "A latte" from the friend's purchased array.
+console.log(user.friend.purchased[1]);
+// F. Loops
+// Write a for loop that iterates over the User's purchased array (NOT the friend's purchased array), and prints each element to the console.
+for (const key in user.purchased) {
+    console.log(user.purchased[key]);
+}
+// Write a for loop that iterates over the Friend's purchased array, and prints each element to the console.
+for (const key in user.friend.purchased) {
+    console.log(user.friend.purchased[key]);
+}
